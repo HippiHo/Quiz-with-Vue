ed4d8e1e1dfc16b62f4c9f8cabc00eee355ff9db
Elise Maschke <elise.maschke@outlook.de>
2019-09-04 17:41:35 +0200

    Display API content

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,19 +1,44 @@
 <template>
   <div class="question-box-container">
     <b-jumbotron>
-
-      <template slot="lead">
-        Some question
-      </template>
+      <template slot="lead">{{ currentQuestion.question }}</template>
 
       <hr class="my-4" />
 
-      <p>
-        List of answers
-      </p>
+      <b-list-group>
+        <b-list-group-item v-for="(answer, index) in answers" :key="index ">{{ answer }}</b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#">Next</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
+  },
+  mounted() {
+    console.log("current Answer", this.currentQuestion);
+  }
+};
+</script>
+
+<style scoped>
+  .list-group {
+      margin-bottom: 1rem;
+  }
+  .btn {
+    margin: 0 0.5rem;
+  }
+</style>
