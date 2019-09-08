eae1014abc77a38e871eea6b7686f48a969ff8ab
Elise Maschke <elise.maschke@outlook.de>
2019-09-08 23:52:46 +0200

    added button functionality and colors

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -6,20 +6,39 @@
       <hr class="my-4" />
 
       <b-list-group>
-        <b-list-group-item v-for="(answer, index) in answers" :key="index ">{{ answer }}</b-list-group-item>
+        <b-list-group-item
+          v-for="(answer, index) in shuffledAnswers"
+          :key="index "
+          @click="selectAnswer(index)"
+          :class="answerClass(index)"
+        >{{ answer }}</b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" href="#">Submit</b-button>
-      <b-button @click="next" variant="success" href="#">Next</b-button>
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || answered"
+      >Submit</b-button>
+      <b-button @click="next" variant="success">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
 
 <script>
+import _ from "lodash";
 export default {
   props: {
     currentQuestion: Object,
-    next: Function
+    next: Function,
+    increment: Function
+  },
+  data() {
+    return {
+      selectedIndex: null,
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
+    };
   },
   computed: {
     answers() {
@@ -28,17 +47,89 @@ export default {
       return answers;
     }
   },
+  watch: {
+    currentQuestion: {
+      //  instead of doing shuffleAnswers() also in mounted() immediate activates even when
+      //  currentQuestion gets passed as props first
+      immediate: true,
+      handler() {
+        this.selectedIndex = null;
+        this.shuffleAnswers();
+        this.answered = false;
+      }
+    }
+    //   currentQuestion() {
+    //   this.selectedIndex = null;
+    //   this.shuffleAnswers()
+    // }
+  },
+  methods: {
+    selectAnswer(index) {
+      this.selectedIndex = index;
+    },
+    submitAnswer() {
+      let isCorrect = false;
+
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true;
+      }
+
+      this.answered = true;
+
+      this.increment(isCorrect);
+    },
+    shuffleAnswers() {
+      let answers = [
+        ...this.currentQuestion.incorrect_answers,
+        this.currentQuestion.correct_answer
+      ];
+      this.shuffledAnswers = _.shuffle(answers);
+      this.correctIndex = this.shuffledAnswers.indexOf(
+        this.currentQuestion.correct_answer
+      );
+    },
+    answerClass(index) {
+      let answerClass = "";
+
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = "selected";
+      } else if (this.answered && this.correctIndex === index) {
+        answerClass = "correct";
+      } else if (
+        this.answered &&
+        this.selectedIndex === index &&
+        this.correctIndex !== index
+      ) {
+        answerClass = "incorrect";
+      }
+
+      return answerClass;
+    }
+  },
   mounted() {
-    console.log("current Answer", this.currentQuestion);
+    // this.shuffleAnswers()
   }
 };
 </script>
 
 <style scoped>
-  .list-group {
-      margin-bottom: 1rem;
-  }
-  .btn {
-    margin: 0 0.5rem;
-  }
+.list-group {
+  margin-bottom: 1rem;
+}
+.list-group-item:hover {
+  background-color: #eee;
+  cursor: pointer;
+}
+.selected {
+  background-color: lightblue;
+}
+.correct {
+  background-color: lightgreen;
+}
+.incorrect {
+  background-color: lightcoral;
+}
+.btn {
+  margin: 0 0.5rem;
+}
 </style>
